b9a598c21f2501fc28d1f0979195c827222a9dd1
Sable Seyler <sable@seyleri.us>
2020-12-29 09:53:42 -0600

    Add initial version of warp

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # warp
-Highlight and locate NONL non-local entanglements in comments and strings
+Highlight and locate NONL non-local entanglements in comments and strings.
+
+It's very easy to wind up with entangled, interdependent implementations.
+Without careful tracking, 
+one can quickly wind up with technical debt that is thoroughly wedged, 
+and be unable to straight-forwardly locate all the necessary changes
+to replace the implementation with something better.
+
+To fix this, consider the Non-Local Entanglement comment flag. 
+Just like one might normally mark code with `TODO:` or `FIXME:`,
+one can start a comment with `NONL:` and some identifying string.
+This identifier is then used to mark each entangled piece of code.
+For example:
+
+``` emacs-lisp
+;; NONL: Terrible Kludge
+(progn 
+  (do-something-terrible)
+  (do-it-fast))
+```
+
+With `warp-mode` enabled, Emacs will highlight `NONL` in constant face,
+and turn `Terrible Kludge` into a button that launches a search for that NONL,
+across your entire project. 
+
+`warp` depends on `projectile` and `rg`, 
+and can be added to Doom Emacs with the following snippets:
+
+``` emacs-lisp $DOOMDIR/packages.el
+(package! rg)
+
+(package! warp
+:recipe (:host github :repo "seylerius/warp"))
+```
+
+``` emacs-lisp $DOOMDIR/config.el
+(use-package! rg
+  :config
+  (setq rg-group-result nil)
+  :commands (rg))
+
+(use-package! warp
+  :hook (prog-mode . warp-mode))
+```
+
---
+++ warp.el
@@ -0,0 +1,161 @@
+;;; warp.el --- description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2020 Sable Seyler
+;;
+;; Author: Sable Seyler <http://github.com/seylerius>
+;; Maintainer: Sable Seyler <sable@seyleri.us>
+;; Created: December 29, 2020
+;; Modified: December 29, 2020
+;; Version: 0.1.0
+;; Keywords:
+;; Homepage: https://github.com/seylerius/warp
+;; Package-Requires: ((emacs 27.1) (cl-lib "0.5"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+;;
+;;
+;;; Commentary:
+;;
+;; Highlight and locate NONL non-local entanglements in comments and strings.
+;;
+;; You can either explicitly turn on `warp-mode' in specific buffers or use the
+;; global variant `global-warp-mode', which enables the local mode based on each
+;; buffer's major-mode and the options `warp-include-modes' and
+;; `warp-exclude-modes'. By default, `warp-mode' is enabled for all buffers
+;; whose major-mode derive from either `prog-mode'.
+;;
+;; Non-Local entanglements, indicated by a comment containing `NONL:' followed
+;; by a text key, are used to declare that a piece of code depends on the
+;; implementation consistency of one or more different pieces of code also
+;; marked with that same key. When enabled, `warp-mode' will turn each such line
+;; into a button, clickable with `mouse-2', that initiates a project-wide search
+;; for all other locations with the same key.
+;;
+;; Substantial inspiration and some code borrowed from Jonas Bernoulli's
+;; excellent `hl-todo-mode', which demonstrated quite well how to add highlights
+;; to existing modes and detect whether one is currently in a comment or string.
+;;
+;;; Code:
+(require 'rx)
+(require 'cl-lib)
+(require 'rg)
+(require 'projectile)
+
+(defgroup warp nil
+  "Highlight and locate NONL non-local entanglements in comments and strings."
+  :group 'font-lock-extra-types)
+
+(defcustom warp-include-modes '(prog-mode)
+  "Major-modes in which `warp-mode' is activated."
+  :group 'warp
+  :type '(repeat function))
+
+(defcustom warp-exclude-modes '()
+  "Major-modes in which `warp-mode' is not activated."
+  :group 'warp
+  :type '(repeat function))
+
+(defvar warp--syntax-table (copy-syntax-table text-mode-syntax-table))
+
+(defvar warp-mode-map (make-sparse-keymap)
+  "Keymap for `warp-mode'.")
+
+(defun warp--inside-comment-or-string-p ()
+  "Check whether point is inside a comment or string."
+  (nth 8 (syntax-ppss)))
+
+(defconst warp--non-local-regexp
+  (rx-to-string '(seq (group-n 1 "NONL:") (* space) (group-n 2 (* not-newline)))))
+
+(defun warp--search (&optional regexp bound backward)
+  "Search (forward unless BACKWARD non-nil) to BOUND for REGEXP."
+  (unless regexp
+    (setq regexp warp--non-local-regexp))
+  (cl-block nil
+    (while (let ((case-fold-search nil))
+             (with-syntax-table warp--syntax-table
+               (funcall (if backward #'re-search-backward #'re-search-forward)
+                        regexp bound t)))
+      (cond ((warp--inside-comment-or-string-p)
+             (cl-return t))
+            ((and bound (funcall (if backward #'<= #'>=) (point) bound))
+             (cl-return nil))))))
+
+(defvar warp--keywords '(((lambda (bound) (warp--search nil bound))
+                          (1 font-lock-constant-face t t)
+                          (2 font-lock-string-face t t)))
+  "Keyword matcher for `warp-mode'.")
+
+(defun warp--setup ()
+  "Initialize font-lock keywords."
+  (font-lock-add-keywords nil warp--keywords))
+
+(defun warp--rg-nonl-regexp (identifier)
+  "Generate a ripgrep regexp matching the Non-Local key IDENTIFIER."
+  (concat "NONL:[ \\t\\f]*" identifier))
+
+(defun warp--call-ripgrep (identifier)
+  "Call ripgrep to locate the Non-Local key IDENTIFIER."
+  (let ((rg-group-result nil))
+    (rg-run
+     (warp--rg-nonl-regexp identifier)
+     "everything"
+     (or (projectile-project-root) default-directory)
+     nil nil
+     '("-n" "-H" "--no-heading"))))
+
+(defun warp--locate (button)
+  "Call ripgrep for the Non-Local key in BUTTON."
+  (warp--call-ripgrep (button-label button)))
+
+(define-button-type 'warp-button
+  'action #'warp--locate
+  'help-echo "mouse-2, RET: Locate instances of this Non-Local entanglement key"
+  'supertype 'button)
+
+;;;###autoload
+(define-minor-mode warp-mode
+  "Highlight and locate NONL non-local entanglements in comments and strings."
+  :lighter ""
+  :keymap warp-mode-map
+  :group 'warp
+  (if warp-mode
+      (warp--setup)
+    (font-lock-remove-keywords nil warp--keywords))
+  (when font-lock-mode
+    (save-excursion
+      (goto-char (point-min))
+      (while (warp--search)
+        (save-excursion
+          (font-lock-fontify-region (match-beginning 0) (match-end 0) nil)))
+      (goto-char (point-min))
+      (while (warp--search)
+        (save-excursion
+          (make-button (match-beginning 2) (match-end 2) 'type 'warp-button))))))
+
+;;;###autoload
+(define-globalized-minor-mode global-warp-mode
+  warp-mode warp--turn-on-mode-if-desired)
+
+(defun warp--turn-on-mode-if-desired ()
+  "Check for whether we're in a desired mode and turn on `warp-mode' if so."
+  (when (and (apply #'derived-mode-p warp-include-modes)
+             (not (apply #'derived-mode-p warp-exclude-modes))
+             (not (bound-and-true-p enriched-mode)))
+    (warp-mode 1)))
+
+(provide 'warp)
+;;; warp.el ends here
